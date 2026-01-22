bb4a17698a02ce86bd712b6d4c8fef9fc1ce7e14
Marwan <marwanmohamed5174@gmail.com>
2026-01-22 14:53:03 +0200

    added globale Exception handler #8-2

--- TeamsManagmentProject/Middleware/GlobalHandlingMiddleware.cs
+++ TeamsManagmentProject/Middleware/GlobalHandlingMiddleware.cs
@@ -1,6 +1,41 @@
-﻿namespace TeamsManagmentProject.API.Middleware
+﻿using Domain.Exceptions;
+using System;
+using System.Net;
+using System.Text.Json;
+
+namespace TeamsManagmentProject.API.Middleware
 {
-    public class GlobalHandlingMiddleware
+    public class GlobalHandlingMiddleware(RequestDelegate _next,ILogger<GlobalHandlingMiddleware> _logger)
     {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong:{ex.Message}");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = ex switch
+            {
+                NotFoundException => (int)HttpStatusCode.NotFound,
+                BadRequestException => (int)HttpStatusCode.BadRequest,
+                ConflictException => (int)HttpStatusCode.Conflict,
+                ForbiddenException => (int)HttpStatusCode.Forbidden,
+                UnauthorizedException => (int)HttpStatusCode.Unauthorized,
+                InternalServerErrorException => (int)HttpStatusCode.InternalServerError,
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+
+            context.Response.ContentType = "application/json";
+
+            var result = JsonSerializer.Serialize(new { error = ex.Message });
+            return context.Response.WriteAsync(result);
+        }
     }
 }
